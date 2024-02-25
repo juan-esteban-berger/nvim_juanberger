278594138001726cd5d63cfe1579afcc4781fbf2
juan-esteban-berger <juanestebanberger@gmail.com>
2024-02-25 07:27:19 -0600

    Commented image

--- nvim/init.lua
+++ nvim/init.lua
@@ -102,38 +102,38 @@ vim.defer_fn(function()
   -- )
 
   -- default config
-  require("image").setup({
-    backend = "kitty",
-    integrations = {
-      markdown = {
-        enabled = true,
-        clear_in_insert_mode = false,
-        download_remote_images = true,
-        only_render_image_at_cursor = false,
-        filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
-      },
-      neorg = {
-        enabled = true,
-        clear_in_insert_mode = false,
-        download_remote_images = true,
-        only_render_image_at_cursor = false,
-        filetypes = { "norg" },
-      },
-    },
-    max_width = nil,
-    max_height = nil,
-    max_width_window_percentage = nil,
-    max_height_window_percentage = 50,
-    -- toggles images when windows are overlapped
-    window_overlap_clear_enabled = false,
-    window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
-    -- auto show/hide images when the editor gains/looses focus
-    editor_only_render_when_focused = false,
-    -- auto show/hide images in the correct Tmux window (needs visual-activity off)
-    tmux_show_only_in_active_window = false,
-    -- resize image files as images when opened
-    hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp" },
-  })
+  -- require("image").setup({
+  --   backend = "kitty",
+  --   integrations = {
+  --     markdown = {
+  --       enabled = true,
+  --       clear_in_insert_mode = false,
+  --       download_remote_images = true,
+  --       only_render_image_at_cursor = false,
+  --       filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
+  --     },
+  --     neorg = {
+  --       enabled = true,
+  --       clear_in_insert_mode = false,
+  --       download_remote_images = true,
+  --       only_render_image_at_cursor = false,
+  --       filetypes = { "norg" },
+  --     },
+  --   },
+  --   max_width = nil,
+  --   max_height = nil,
+  --   max_width_window_percentage = nil,
+  --   max_height_window_percentage = 50,
+  --   -- toggles images when windows are overlapped
+  --   window_overlap_clear_enabled = false,
+  --   window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
+  --   -- auto show/hide images when the editor gains/looses focus
+  --   editor_only_render_when_focused = false,
+  --   -- auto show/hide images in the correct Tmux window (needs visual-activity off)
+  --   tmux_show_only_in_active_window = false,
+  --   -- resize image files as images when opened
+  --   hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp" },
+  -- })
   vim.cmd("highlight Normal guibg=black")
   vim.cmd("highlight Comment guifg=#5AE3FF")
   vim.cmd("highlight LineNr guifg=White")
